82d217563916586bf43e58e7470c7c2a84984c37
Daniele Madama <d.madama@gmail.com>
2014-05-21 15:20:38 +0200

    Remove unused import

--- src/main/java/net/etalia/client/http/Caller.java
+++ src/main/java/net/etalia/client/http/Caller.java
@@ -6,7 +6,6 @@ import java.lang.reflect.Method;
 import java.lang.reflect.Proxy;
 import java.lang.reflect.Type;
 import java.util.ArrayList;
-import java.util.Collection;
 import java.util.List;
 import java.util.Stack;
 
