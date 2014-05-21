84959533437665b7474902617dc4b631ed77af74
Daniele Madama <d.madama@gmail.com>
2014-05-21 16:12:30 +0200

    Add extraData to User

--- src/main/java/net/etalia/client/domain/User.java
+++ src/main/java/net/etalia/client/domain/User.java
@@ -1,9 +1,13 @@
 package net.etalia.client.domain;
 
+import java.util.Collections;
+import java.util.Map;
+
 public class User {
 
 	private String id;
 	private String title;
+	private Map<String, Object> extraData;
 
 	public String getId() {
 		return id;
@@ -16,6 +20,15 @@ public class User {
 	}
 	public void setTitle(String title) {
 		this.title = title;
+	}
+	public Map<String,Object> getExtraData() {
+		if (this.extraData == null) return Collections.emptyMap();
+		return Collections.unmodifiableMap(this.extraData);
+	}
+	@SuppressWarnings("unchecked")
+	public <T> T getExtraData(String name) {
+		if (extraData == null) return null;
+		return (T)extraData.get(name);
 	}
 
 }
