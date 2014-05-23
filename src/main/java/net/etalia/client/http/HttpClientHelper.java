55715b0a4dc9a60c48e29d6299a199518502f59d
Daniele Madama <d.madama@gmail.com>
2014-05-23 14:17:52 +0200

    Create HttpClient for SSL

--- src/main/java/net/etalia/client/http/HttpClientHelper.java
+++ src/main/java/net/etalia/client/http/HttpClientHelper.java
@@ -3,7 +3,11 @@ package net.etalia.client.http;
 import java.util.concurrent.TimeUnit;
 
 import org.apache.http.client.HttpClient;
+import org.apache.http.client.config.RequestConfig;
+import org.apache.http.conn.ssl.SSLConnectionSocketFactory;
+import org.apache.http.impl.client.CloseableHttpClient;
 import org.apache.http.impl.client.DefaultHttpClient;
+import org.apache.http.impl.client.HttpClientBuilder;
 import org.apache.http.impl.conn.PoolingClientConnectionManager;
 import org.apache.http.impl.conn.SchemeRegistryFactory;
 
@@ -26,5 +30,17 @@ public class HttpClientHelper {
 		ret.getParams().setParameter("http.protocol.head-body-timeout", timeout);
 		return ret;
 	}
-	
+
+	public HttpClient createDefaultClient(int connections, int timeout, SSLConnectionSocketFactory sslsf) {
+		HttpClientBuilder clientBuilder = HttpClientBuilder.create();
+		clientBuilder.setSSLSocketFactory(sslsf);
+		clientBuilder.setMaxConnPerRoute(connections);
+		clientBuilder.setMaxConnTotal(connections);
+		RequestConfig.Builder requestBuilder = RequestConfig.custom();
+		requestBuilder.setConnectTimeout(timeout);
+		requestBuilder.setConnectionRequestTimeout(timeout);
+		clientBuilder.setDefaultRequestConfig(requestBuilder.build());
+		CloseableHttpClient client = clientBuilder.build();
+		return client;
+	}
 }
