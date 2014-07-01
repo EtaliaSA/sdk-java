694c550aadfff8c0f5713987fcd42e8c6d683612
Daniele Madama <d.madama@gmail.com>
2014-07-01 15:46:35 +0200

    Add missing id property

--- src/main/java/net/etalia/client/domain/JournaliaStampAggregation.java
+++ src/main/java/net/etalia/client/domain/JournaliaStampAggregation.java
@@ -2,4 +2,14 @@ package net.etalia.client.domain;
 
 public class JournaliaStampAggregation<T extends Publication> extends JournaliaAggregation<T> {
 
+	private String id;
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
 }
