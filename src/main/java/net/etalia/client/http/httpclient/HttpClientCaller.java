212f03ea8e7e79d084d63a113524c81f99d2e3b3
Daniele Madama <d.madama@gmail.com>
2014-07-04 14:18:05 +0200

    Clean code

--- src/main/java/net/etalia/client/http/httpclient/HttpClientCaller.java
+++ src/main/java/net/etalia/client/http/httpclient/HttpClientCaller.java
@@ -4,7 +4,6 @@ import java.lang.reflect.Type;
 
 import net.etalia.client.http.Call;
 import net.etalia.client.http.Caller;
-import net.etalia.client.http.Caller.HttpMethod;
 
 import org.apache.http.client.HttpClient;
 
@@ -12,21 +11,6 @@ public class HttpClientCaller<Serv> extends Caller<Serv> {
 
 	protected HttpClient httpClient;
 	protected String baseUrl;
-	
-	public void setHttpClient(HttpClient httpClient) {
-		this.httpClient = httpClient;
-	}
-	
-	public HttpClient getHttpClient() {
-		return httpClient;
-	}
-	
-	public void setBaseUrl(String baseUrl) {
-		this.baseUrl = baseUrl;
-	}
-	public String getBaseUrl() {
-		return baseUrl;
-	}
 	
 	public HttpClientCaller(Class<Serv> serviceInterface) {
 		super(serviceInterface);
@@ -37,6 +21,18 @@ public class HttpClientCaller<Serv> extends Caller<Serv> {
 		return new HttpClientCall<X>(this, type, method, path);
 	}
 
-	
-	
+	public void setHttpClient(HttpClient httpClient) {
+		this.httpClient = httpClient;
+	}
+	public HttpClient getHttpClient() {
+		return httpClient;
+	}
+
+	public void setBaseUrl(String baseUrl) {
+		this.baseUrl = baseUrl;
+	}
+	public String getBaseUrl() {
+		return baseUrl;
+	}
+
 }
