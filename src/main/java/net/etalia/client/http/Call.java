a49ed441dc71f1df45b4bfa68b3da924464f76ea
Simone Gianni <simoneg@apache.org>
2014-07-15 13:14:40 +0200

    Accept 200 only on GET and accept only 201 on POST

--- src/main/java/net/etalia/client/http/Call.java
+++ src/main/java/net/etalia/client/http/Call.java
@@ -37,6 +37,10 @@ public abstract class Call<Ret> {
 			BitSet bs = new BitSet(600);
 			bs.set(200);
 			DEFAULT_ACCEPTS.put(HttpMethod.GET, bs);
+		}
+		{
+			BitSet bs = new BitSet(600);
+			bs.set(201);
 			DEFAULT_ACCEPTS.put(HttpMethod.POST, bs);
 		}
 		{
